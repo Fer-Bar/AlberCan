d3c6fca1ee78c305a903d251fccaff8be9683e2d
Fer-Bar <linofernando2703@gmail.com>
2023-05-07 23:42:33 -0400

    Fixed carousel imports

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-//Slider Initialzation
+const loadCarousel = () => {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 10,
+        items: 1,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 1,
+                nav: false,
+            },
+            1000: {
+                items: 1,
+                nav: true,
+            },
+        }
+    })
+}
 
-$('.home-slider').owlcarousel({
-    loop: true,
-    autoplay: true,
-    margin: 10,
-    autoplayHoverPause: true,
-    items: 1,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    navText: ["<span class= 'fa fa-chevron-left'></span>","<span class= 'fa fa-chevron-right'></span>"],
-    responsive: {
-        0: {
-            items: 1,
-            nav: false,
-        },
-        600: {
-            items: 1,
-            nav: false,
-        },
-        1000: {
-            items: 1,
-            nav: true,
-        },
-    }
-})
-
+$(document).ready(function () {
+    loadCarousel();
+  })

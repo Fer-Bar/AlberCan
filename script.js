ea002b220888a7c41f294b55210f19a84ada5ac7
Fer-Bar <linofernando2703@gmail.com>
2023-05-18 15:10:18 -0400

    Fixed nav button in secundary carousel

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-const loadCarousel = () => {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        margin: 10,
-        items: 1,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        nav: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-            },
-            600: {
-                items: 2,
-                nav: true,
-            },
-            1000: {
-                items: 3,
-                nav: true,
-            },
-        }
-    })
-}
-
-$(document).ready(function () {
-    loadCarousel();
-  })
-  // video 3
-  $('.js-carousel').owlCarousel({
+$('.home-slider.owl-carousel').owlCarousel({
     loop: true,
     autoplay: true,
-    stagePadding:7,
-    margin: 20,
-    animateOut: fadeOut,
-    animateIn: fadeIn,
+    margin: 10,
+    items: 1,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
     nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
-    responsive:{
-        0:{
+    navText: ["<span class='fa-solid fa-chevron-left'></span>", "<span class='fa-solid fa-chevron-right'></span>"],
+    responsive: {
+        0: {
             items: 1,
             nav: false,
         },
-        600:{
-            items: 2,
-            nav: false,
+        600: {
+            items: 1,
+            nav: true,
         },
-        1000:{
-            items: 3,
-            nav: false,
-            loop: false,
-        }
+        1000: {
+            items: 1,
+            nav: true,
+        },
     }
-  })
+})
+
+$('.major-carousel.js-carousel.owl-carousel').owlCarousel({
+  loop: true,
+  autoplay: true,
+  stagePadding:7,
+  margin: 20,
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
+  nav: true,
+  autoplayHoverPause: true,
+  items: 3,
+  navText: ["<span class='fa-solid fa-chevron-left'></span>", "<span class='fa-solid fa-chevron-right'></span>"],
+  responsive:{
+      0:{
+          items: 1,
+          nav: false,
+      },
+      600:{
+          items: 2,
+          nav: false,
+      },
+      1000:{
+          items: 3,
+          nav: true,
+          loop: true,
+      }
+  }
+})
+
+// const loadMainCarousel = () => {
+// }
+
+//   // video 3
+// const loadSecundaryCarousel = () => {
+// }
+
+// $(document).ready(function () {
+//     loadMainCarousel();
+//     loadSecundaryCarousel();
+//   })

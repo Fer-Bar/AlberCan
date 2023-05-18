29ac59085771e6b1bba4a78a5e75652094d7d453
enrker <enriquecuellar987@gmail.com>
2023-05-18 14:44:07 -0400

    Diseñe un control deslizante de medios de imagen

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ const loadCarousel = () => {
 $(document).ready(function () {
     loadCarousel();
   })
+  // video 3
+  $('.js-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    stagePadding:7,
+    margin: 20,
+    animateOut: fadeOut,
+    animateIn: fadeIn,
+    nav: true,
+    autoplayHoverPause: true,
+    items: 3,
+    navText: ["<span class='fa fa-chevron-left'></span>", "<span class='fa fa-chevron-right'></span>"],
+    responsive:{
+        0:{
+            items: 1,
+            nav: false,
+        },
+        600:{
+            items: 2,
+            nav: false,
+        },
+        1000:{
+            items: 3,
+            nav: false,
+            loop: false,
+        }
+    }
+  })
